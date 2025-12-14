ea0ef9d71f2296ce2845d1ca7d6b01de7091bc74
Essayas Abraham Hailu <80784659+tedy-abr@users.noreply.github.com>
2025-12-14 22:48:59 +0100

    Use relative paths for build input

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,18 @@
 import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
-import { resolve } from "path";
 
 export default defineConfig({
   plugins: [tailwindcss()],
   build: {
     rollupOptions: {
       input: {
-        main: resolve(__dirname, "index.html"),
-        login: resolve(__dirname, "login.html"),
-        register: resolve(__dirname, "register.html"),
-        profile: resolve(__dirname, "profile.html"),
-        details: resolve(__dirname, "details.html"),
-        create: resolve(__dirname, "create-listing.html"),
-        edit: resolve(__dirname, "edit-profile.html"),
+        main: "./index.html",
+        login: "./login.html",
+        register: "./register.html",
+        profile: "./profile.html",
+        details: "./details.html",
+        create: "./create-listing.html",
+        edit: "./edit-profile.html",
       },
     },
   },
