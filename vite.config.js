8dd43f849117aa442de1b0233a384fbd9b6b7fe4
Essayas Abraham Hailu <80784659+tedy-abr@users.noreply.github.com>
2025-12-14 22:34:35 +0100

    Configure Vite for multi-page build

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,20 @@
-import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import tailwindcss from "@tailwindcss/vite";
+import { resolve } from "path";
 
 export default defineConfig({
-  plugins: [
-    tailwindcss(),
-  ],
-})
+  plugins: [tailwindcss()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        login: resolve(__dirname, "login.html"),
+        register: resolve(__dirname, "register.html"),
+        profile: resolve(__dirname, "profile.html"),
+        details: resolve(__dirname, "details.html"),
+        create: resolve(__dirname, "create-listing.html"),
+        edit: resolve(__dirname, "edit-profile.html"),
+      },
+    },
+  },
+});
